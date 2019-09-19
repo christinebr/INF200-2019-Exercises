ad29d2691029ca0889699e8a5dd13de40f5b5bde
Christine Brinchmann <christibr@nmbu.no>
2019-09-19 18:51:19 +0200

    Added doc-string to the function squares_by_loop()

--- src/christine_brinchmann_ex/ex01/comp_to_loop.py
+++ src/christine_brinchmann_ex/ex01/comp_to_loop.py
@@ -1,6 +1,9 @@
 
 
 def squares_by_loop(n):
+    """ Returns a list with the squared value of the numbers in range(n),
+    for which the remainder of a division by 3 is equal to 1.
+    """
     squares = []
     for k in range(n):
         if k % 3 == 1:
