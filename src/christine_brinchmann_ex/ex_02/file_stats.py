e7e0cd89014f02ad10c4cb3b2e84a619a725b4db
Christine Brinchmann <christibr@nmbu.no>
2019-09-26 12:45:03 +0200

    Wrote code for the function char_counts()

--- src/christine_brinchmann_ex/ex_02/file_stats.py
+++ src/christine_brinchmann_ex/ex_02/file_stats.py
@@ -1,7 +1,33 @@
+from src.christine_brinchmann_ex.ex01.letter_counts import letter_freq
 
 
 def char_counts(textfilename):
-    pass
+    """
+
+    Parameters
+    ----------
+    textfilename: the filename of the file which is opened using utf-8
+    encoding. The function then reads the entire file into one string.
+
+    Returns
+    -------
+    A list with the occurrences of character codes, between 0 and 255, in the
+    file opened. char_code_counts[i] gives the occurrences of character code i.
+
+    """
+
+    infile = open(textfilename, 'r', encoding='utf-8')
+    single_string = infile.read()
+    infile.close()
+
+    freq_char = letter_freq(single_string)
+
+    char_code_counts = [0]*256
+    for letter, count in freq_char.items():
+        index = ord(letter)
+        char_code_counts[index] = count
+
+    return char_code_counts
 
 
 if __name__ == '__main__':
