34ee770724edb5f6e9ec10268afd4d5628a6880d
Christine Brinchmann <christibr@nmbu.no>
2019-09-27 09:23:51 +0200

    Wrote code for the function bubble_sort()

--- src/christine_brinchmann_ex/ex_02/bubble_sort.py
+++ src/christine_brinchmann_ex/ex_02/bubble_sort.py
@@ -1,6 +1,31 @@
+# -*- coding: utf-8 -*-
 
-def bubble_sort(data):
-    pass
+__author__ = "Christine Brinchmann"
+__email__ = "christibr@nmbu.no"
+
+
+def bubble_sort(data_input):
+    """
+    The function sorts data using the bubble sort method.
+
+    Parameters
+    ----------
+    data_input: the data to be sorted.
+
+    Returns
+    -------
+    A new tuple with the data in sorted order.
+
+    """
+    new_data = list(data_input)
+    n = len(new_data)
+
+    for i in range(n-1):
+        for j in range(n-1-i):
+            if new_data[j] > new_data[j+1]:
+                new_data[j], new_data[j+1] = new_data[j+1], new_data[j]
+
+    return tuple(new_data)
 
 
 if __name__ == "__main__":
