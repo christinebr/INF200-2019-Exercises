9e1fae0dd725b9a014d97e0b72b58a2a2d96dac5
Christine Brinchmann <christibr@nmbu.no>
2019-10-03 17:32:36 +0200

    Modified code for the function median()

--- src/christine_brinchmann_ex/ex03_project/test_median.py
+++ src/christine_brinchmann_ex/ex03_project/test_median.py
@@ -13,9 +13,16 @@ def median(data):
 
     :source of code: https://github.com/yngvem/INF200-2019-Exercises/blob
     /master/exersices/ex03.rst
+
+    The code has been modified
     """
 
-    sdata = sorted(data)
-    n = len(sdata)
-    return (sdata[n//2] if n % 2 == 1
-        else 0.5 * (sdata[n//2 - 1] + sdata[n//2]))
+    sorted_data = sorted(data)
+    num_elements = len(sorted_data)
+
+    if num_elements % 2 == 1:
+        return sorted_data[num_elements // 2]
+    else:
+        return (
+            sorted_data[num_elements // 2 - 1] + sorted_data[num_elements // 2]
+            ) / 2
