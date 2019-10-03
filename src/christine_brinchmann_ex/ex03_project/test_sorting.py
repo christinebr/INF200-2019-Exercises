2106ecb48fb94a2d8ea1c744755b227b883f484b
Christine Brinchmann <christibr@nmbu.no>
2019-10-03 17:59:47 +0200

    Copied the function bubble_sort() from ex02 into test_sorting.py

---
+++ src/christine_brinchmann_ex/ex03_project/test_sorting.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Christine Brinchmann"
+__email__ = "christibr@nmbu.no"
+
+
+def bubble_sort(data_input):
+    """
+    The function sorts data using the bubble sort method.
+
+    Parameters
+    ----------
+    data_input: the data to be sorted.
+
+    Returns
+    -------
+    A new list with the data in sorted order.
+
+    """
+    new_data = list(data_input)
+    n = len(new_data)
+
+    for i in range(n-1):
+        for j in range(n-1-i):
+            if new_data[j] > new_data[j+1]:
+                new_data[j], new_data[j+1] = new_data[j+1], new_data[j]
+
+    return new_data
