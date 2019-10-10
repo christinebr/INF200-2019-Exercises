5962da6ca8de1c1bae273ae3c645438d5424edbb
Christine Brinchmann <christibr@nmbu.no>
2019-10-10 13:35:11 +0200

    Added the snakes and ladders for the game

--- src/pa01/snakes_and_ladders.py
+++ src/pa01/snakes_and_ladders.py
@@ -2,6 +2,24 @@ __author__ = 'Christine Brinchmann', 'Marie Kolvik Valøy'
 __email__ = 'christibr@nmbu.no', 'mvaloy@nmbu.no'
 
 
+ladders_snakes = {
+        1: 40,
+        8: 10,
+        36: 52,
+        43: 62,
+        49: 79,
+        65: 82,
+        68: 85,
+        24: 5,
+        33: 3,
+        42: 30,
+        56: 37,
+        64: 27,
+        74: 12,
+        87: 70
+        }
+
+
 def single_game(num_players):
     """
     Returns duration of single game.
