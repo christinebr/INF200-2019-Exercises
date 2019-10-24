75d6155938a9c147f1d56c62a774a284d593c462
Christine Brinchmann <christibr@nmbu.no>
2019-10-24 08:46:18 +0200

    Created class LCGRand in myrand.py

--- src/christine_brinchmann_ex/ex04/myrand.py
+++ src/christine_brinchmann_ex/ex04/myrand.py
@@ -2,3 +2,14 @@
 
 __author__ = "Christine Brinchmann"
 __email__ = "christibr@nmbu.no"
+
+
+class LCGRand:
+    def __init__(self, seed):
+        self.r = seed
+
+    def rand(self):
+        a = 7**5
+        m = 2**31-1
+        self.r = (a * self.r) % m
+        return self.r
