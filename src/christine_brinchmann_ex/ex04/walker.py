4ad85f6e8dfb8ff67f675033345caafede5d1777
Christine Brinchmann <christibr@nmbu.no>
2019-10-24 09:50:39 +0200

    Created function walking_process and added a __main__ block in walker.py

--- src/christine_brinchmann_ex/ex04/walker.py
+++ src/christine_brinchmann_ex/ex04/walker.py
@@ -28,3 +28,32 @@ class Walker:
 
     def get_steps(self):
         return self.steps
+
+
+def walking_process(start_position, distance_home, num_simulations):
+    num_steps = []
+    for i in range(num_simulations):
+
+        student = Walker(start_position, distance_home)
+
+        while student.is_at_home() is False:
+            student.move()
+
+        num_steps.append(student.get_steps())
+
+    return num_steps
+
+
+if __name__ == '__main__':
+    start = 0
+    distances = [1, 2, 5, 10, 20, 50, 100]
+    num_sim = 5
+
+    steps = []
+    for distance in distances:
+        steps.append(walking_process(start, distance, num_sim))
+
+    for distance, step in zip(distances, steps):
+        sorted_step = sorted(step)
+        print('Distance: {0:3d} -> Path lengths: {1}'.format(distance,
+                                                             sorted_step))
