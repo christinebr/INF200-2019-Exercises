37fd53f73c3c343900595b005a53db24c413aa99
Christine Brinchmann <christibr@nmbu.no>
2019-10-24 09:26:00 +0200

    Created class Walker in walker.py

--- src/christine_brinchmann_ex/ex04/walker.py
+++ src/christine_brinchmann_ex/ex04/walker.py
@@ -2,3 +2,29 @@
 
 __author__ = "Christine Brinchmann"
 __email__ = "christibr@nmbu.no"
+
+from random import uniform
+
+
+class Walker:
+    def __init__(self, initial_position, home):
+        self.position = initial_position
+        self.home = home
+        self.steps = 0
+
+    def move(self):
+        self.steps += 1
+        direction = uniform(0, 1)
+        if direction < 0.5:
+            self.position -= 1
+        else:
+            self.position += 1
+
+    def is_at_home(self):
+        return self.position == self.home
+
+    def get_position(self):
+        return self.position
+
+    def get_steps(self):
+        return self.steps
