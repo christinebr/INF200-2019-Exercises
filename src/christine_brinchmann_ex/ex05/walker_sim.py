be69e72ea698699f7c154e001f2c6c08fdcf7324
Christine Brinchmann <christibr@nmbu.no>
2019-10-30 11:35:45 +0100

    Copied the Walker class from ex04 into the file walker_sim.py

--- src/christine_brinchmann_ex/ex05/walker_sim.py
+++ src/christine_brinchmann_ex/ex05/walker_sim.py
@@ -1,4 +1,49 @@
 # -*- coding: utf-8 -*-
+import random
 
 __author__ = "Christine Brinchmann"
 __email__ = "christibr@nmbu.no"
+
+
+class Walker:
+    def __init__(self, initial_position, home):
+        """
+        Parameters
+        ----------
+        initial_position: the start position
+        home: the end position
+
+        Defines number of steps taken as zero.
+        """
+        self.position = initial_position
+        self.home = home
+        self.steps = 0
+
+    def move(self):
+        """
+        Takes a step to the left (-1) or right (+1) from the current position.
+        """
+        self.steps += 1
+        direction = random.uniform(0, 1)
+        if direction < 0.5:
+            self.position -= 1
+        else:
+            self.position += 1
+
+    def is_at_home(self):
+        """
+        Returns True if the end position is reached and False otherwise.
+        """
+        return self.position == self.home
+
+    def get_position(self):
+        """
+        Returns the position.
+        """
+        return self.position
+
+    def get_steps(self):
+        """
+        Returns number of steps taken.
+        """
+        return self.steps
